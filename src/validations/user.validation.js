8f3d5e2be7372ad1350294add683e6589fb9ad70
Joao Kuster <kusterzito@gmail.com>
2023-04-17 01:17:37 -0300

    fix password validation

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -3,6 +3,6 @@ import * as yup from 'yup'
 export const userValidation = yup.object({
   name: yup.string().required(),
   email: yup.string().required().email(),
-  pasword: yup.string().required().min(6),
+  password: yup.string().required().min(6),
 })
 
